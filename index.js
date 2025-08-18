d17545c778c193b58c3ba6a2f660bc243d646838
itzfrizzyy01 <itzfrizzyy01@gmail.com>
2025-08-18 08:31:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mineflayer = require('mineflayer');
+\const mineflayer = require('mineflayer');
 
 function createBot() {
   const bot = mineflayer.createBot({
@@ -13,38 +13,31 @@ function createBot() {
 
   // Auto reconnect
   bot.on('kicked', () => {
-    console.log("Kicked, reconnecting...");
     setTimeout(createBot, 5000);
   });
   bot.on('end', () => {
-    console.log("Disconnected, reconnecting...");
     setTimeout(createBot, 5000);
   });
 
   // Movement loop
   bot.on('spawn', () => {
-    console.log("Bot spawned, starting movement loop...");
-
     let forward = true;
 
     setInterval(() => {
-      // reset all keys first
       bot.clearControlStates();
 
       if (forward) {
-        console.log("Moving forward...");
         bot.setControlState('forward', true);
       } else {
-        console.log("Moving back...");
         bot.setControlState('back', true);
       }
 
-      // jump so it doesn’t get stuck
+      // Jump to avoid getting stuck
       bot.setControlState('jump', true);
       setTimeout(() => bot.setControlState('jump', false), 300);
 
       forward = !forward;
-    }, 3000); // switch direction every 3 seconds
+    }, 3000);
   });
 }
 
