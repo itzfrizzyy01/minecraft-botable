6b6078af01aae35443bd38b1628ecbda8d9fc669
itzfrizzyy01 <itzfrizzyy01@gmail.com>
2025-09-12 07:28:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const PASSWORD = "2211133445"; // your AuthMe password
 
 function createBot() {
   const bot = mineflayer.createBot({
-    host: "1deadsteal.play.hosting",
-    port: 25565,
+    host: "node-my-01.tickhosting.asia",
+    port: 40285,
     username: "mr_trolling",
     version: "1.21"
   });
@@ -62,6 +62,7 @@ http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Bot is running\n");
 }).listen(process.env.PORT || 3000);
+
 
 
 
